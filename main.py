f7f9ed21f43aa9098702f19bba897cd37f37ad7f
Nikolai Varankine <nvaranki@tut.by>
2025-02-21 01:44:39 +0300

    CLI --new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import torch.cuda
-
+import argparse
 from MultiplyModel import MultiplyModel
 from TextDataset import TextDataset
 from torch.nn.utils.rnn import pad_sequence
@@ -10,7 +10,11 @@ from Train import Train
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    ds = TextDataset(Train.generate((1,10),(1,10)))
+    cli = argparse.ArgumentParser(description="Multiplying Table")
+    cli.add_argument("--new", action='store_const', const=True, required=False, default=False, metavar="NEW", help="start with blank model")
+    args = cli.parse_args()
+
+    ds = TextDataset(Train.generate((1,10),(1,10))) # TODO load blank only, use 0 as padding index
     dl = DataLoader(ds, batch_size=5, collate_fn=lambda x: pad_sequence(x, batch_first=True, padding_value=ds.vocab_size))
 
     # Model parameters
@@ -26,7 +30,7 @@ if __name__ == '__main__':
     num_epochs = 10
     learning_rate = 0.001
     trainer = Train(model, ds.vocab_size, learning_rate)
-    weights = trainer.load("data")
+    weights = trainer.load("data") if not args.new else None
     print("New model has been created." if weights is None
           else f"Loaded last saved weights from \"{weights}\" into the model.")
     loss = trainer.run(num_epochs, dl, ds.vocab_size, device)
